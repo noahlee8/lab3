676fd570159d5470c1f06eb75ad5aa64a28abbf3
Noah Lee <nol006@ucsd.edu>
2017-01-26 10:30:58 -0800

    good job

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,30 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("I did it!");
+		$(".jumbotron p").toggleClass("active");
 	});
+	$("a").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	
+  var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+    	var isVisible = $(".project-description").is(":visible")
+    	if (!isVisible){
+    		$(".project-description").show();
+    	}
+    	else{
+       $(".project-description").fadeOut();
+		}
+    }
+
 }
